c6180052ac41f90cebf456b2b7a2492f23755696
macbook <macbook@MacBook-Pro-de-macbook.local>
2021-02-23 20:41:05 -0600

    Add build front react-calendar

---
+++ public/precache-manifest.50cf73d81f43eecf16b6060e0e49c9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea3012eb7a4fd926a921",
+    "url": "/static/css/main.0cee897e.chunk.css"
+  },
+  {
+    "revision": "ea3012eb7a4fd926a921",
+    "url": "/static/js/main.ea3012eb.chunk.js"
+  },
+  {
+    "revision": "f95ddc482e1171f48617",
+    "url": "/static/css/1.0b01e158.chunk.css"
+  },
+  {
+    "revision": "f95ddc482e1171f48617",
+    "url": "/static/js/1.f95ddc48.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "47e49388200b8538f31f905dc646ddf4",
+    "url": "/index.html"
+  }
+];
